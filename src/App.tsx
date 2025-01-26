ac4e687e9f41a00fd036bce605669d5f394c00b6
deian <deian@deians-MBP.verizon.net>
2025-01-26 14:17:38 -0500

    add sample work part 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 import { About } from './components/About';
 import { Footer } from './components/Footer';
+import { SampleWork } from './components/SampleWork';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Hero />
         <About />
         <Experience />
+        <SampleWork />
         <div className="h-32"></div>
       </main>
       <Footer />
